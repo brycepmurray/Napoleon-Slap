766302792f00aaba63dc6383388428c7ea14f298
Bryce Murray <brycepmurray711@gmail.com>
2017-10-27 22:29:14 -0600

    Slap game part two attempt 1

--- game.js
+++ game.js
@@ -1,42 +1,51 @@
-var health = 100;
-var hits = 0
-
-function gameBrains() {
+function gameBrains(slap, nunchuckSkills, bowstaffSkills) {
 
 }
 
-function slap() {
-    health -= 1;
-    hits += 1
+var player = function(health, items, attacks, hits) {
+    this.health = health;
+    this.items = items;
+    this.attacks = attacks;
+    this.hits = hits
+}
+var Napoleon = new player(100, ['grapefruit', 'tots', 'tina'], [slap, nunchuckSkills, bowstaffSkills], 0);
+
+var Rex = new player(100, ['Starla', 'These Bad Boys', 'Break the Wrist, and Walk Away'], [slap, nunchuckSkills, bowstaffSkills], 0)
+
+var attacks = { slap, nunchuckSkills, bowstaffSkills };
+
+function slap(Rex) {
+    Rex.health -= 1;
+    Rex.hits += 1
         //alert(health)
     update()
 }
 
-function nunchuckSkills() {
-    health -= 5;
-    hits += 1;
+function nunchuckSkills(Rex) {
+    Rex.health -= 5;
+    Rex.hits += 1;
     //alert(health)
     update()
 
 }
 
-function bowstaffSkills() {
-    health -= 10;
-    hits += 1;
+function bowstaffSkills(Rex) {
+    Rex.health -= 10;
+    Rex.hits += 1;
     //alert(health)
     update()
 }
 
-//function computerChoice() {
+//function RexChoice() {
 //  var choices = [1, 5, 10]
 //   var num = Math.floor(Math.random() * choices.length);
 //  return choices[num] {
 
 
 function update() {
-    document.getElementById("health").innerText = health
-    document.getElementById("hits").innerText = hits
-    document.getElementById("hits").innerText = hits
-    document.getElementById("health").innerText = health
+    document.getElementById("Napoleon.health").innerText = Napoleon.health;
+    document.getElementById("Napoleon.hits").innerText = Napoleon.hits;
+    document.getElementById("Rex.health").innerText = Rex.health;
+    document.getElementById("Rex.hits").innerText = Rex.hits
 }
 update()
